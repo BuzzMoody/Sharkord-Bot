2846d564edec3356fd7e214456eaea22abaef7ff
Buzz <buzz+github@buzz.id.au>
2026-02-21 17:15:12 +1100

    Update Sharkord.php

--- src/Sharkord.php
+++ src/Sharkord.php
@@ -271,6 +271,7 @@
 			}
 			
 			$this->bot = $this->users->get($raw['ownUserId']);
+			print_r($raw['publicSettings']);
 			$this->servers = $this->servers->handleCreate($raw['publicSettings']);
 
 			$this->logger->info(sprintf("Joined. Cached %d channels, %d users.", $this->channels->count(), $this->users->count()));
